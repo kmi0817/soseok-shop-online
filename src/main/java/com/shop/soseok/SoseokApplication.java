b75b595db6ec7bbcca6da42b0db77068550e9f79
kmi0817 <kmi0817@naver.com>
2025-03-23 22:49:57 +0900

    [feat] start.spring.io

---
+++ src/main/java/com/shop/soseok/SoseokApplication.java
@@ -0,0 +1,13 @@
+package com.shop.soseok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoseokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoseokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/soseok/SoseokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.soseok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoseokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
